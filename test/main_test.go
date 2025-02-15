487e63645ea9c4d137084d47a9f1d332b3008da8
Brandon McNama <brandonmcnama@outlook.com>
2025-02-15 17:27:08 -0500

    feat: add pdb (#7)

--- test/main_test.go
+++ test/main_test.go
@@ -1,3 +1,4 @@
+//nolint:ireturn,thelper
 package main_test
 
 import (
@@ -13,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	policyv1 "k8s.io/api/policy/v1"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -70,6 +72,8 @@ func TestSealedSecrets(t *testing.T) {
 		NamespaceIsRestricted(nsName),
 		CRDExists("sealedsecrets.bitnami.com", "v1alpha1"),
 		DeploymentExists(nsName, deployName),
+		PodDisruptionBudgetExists(nsName, deployName),
+		PodDisruptionBudgetTargetsDeployment(nsName, deployName, deployName),
 		DeploymentIsSystemClusterCritical(nsName, deployName),
 		DeploymentHasCPURequests(nsName, deployName),
 		DeploymentHasNoCPULimits(nsName, deployName),
@@ -202,223 +206,339 @@ func ApplyKustomization(kustDir string) env.Func {
 func NamespaceIsRestricted(namespaceName string) e2etypes.Feature {
 	return features.New("NamespaceIsRestricted").
 		WithLabel("type", "namespace").
-		AssessWithDescription("restrictedNamespace", "Namespace should be restricted", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var ns corev1.Namespace
-			err := cfg.Client().Resources().Get(ctx, namespaceName, "", &ns)
-			require.NoError(t, err)
+		AssessWithDescription(
+			"restrictedNamespace",
+			"Namespace should be restricted",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var ns corev1.Namespace
+				err := cfg.Client().Resources().Get(ctx, namespaceName, "", &ns)
+				require.NoError(t, err)
 
-			nsLabels := ns.GetLabels()
+				nsLabels := ns.GetLabels()
 
-			assert.Contains(t, nsLabels, "pod-security.kubernetes.io/enforce")
-			assert.Equal(t, "restricted", nsLabels["pod-security.kubernetes.io/enforce"])
-			assert.Contains(t, nsLabels, "pod-security.kubernetes.io/audit")
-			assert.Equal(t, "restricted", nsLabels["pod-security.kubernetes.io/audit"])
+				assert.Contains(t, nsLabels, "pod-security.kubernetes.io/enforce")
+				assert.Equal(t, "restricted", nsLabels["pod-security.kubernetes.io/enforce"])
+				assert.Contains(t, nsLabels, "pod-security.kubernetes.io/audit")
+				assert.Equal(t, "restricted", nsLabels["pod-security.kubernetes.io/audit"])
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
 func NamespaceExists(namespaceName string) e2etypes.Feature {
 	return features.New("NamespaceExists").
 		WithLabel("type", "namespace").
-		AssessWithDescription("namespaceExists", "Namespace should exist", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			ns := &corev1.NamespaceList{
-				Items: []corev1.Namespace{{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}},
-			}
+		AssessWithDescription(
+			"namespaceExists",
+			"Namespace should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				ns := &corev1.NamespaceList{
+					Items: []corev1.Namespace{{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}},
+				}
 
-			err := wait.For(conditions.New(cfg.Client().Resources()).ResourcesFound(ns), wait.WithTimeout(3*time.Second), wait.WithImmediate())
-			require.NoError(t, err)
+				err := wait.For(
+					conditions.New(cfg.Client().Resources()).ResourcesFound(ns),
+					wait.WithTimeout(3*time.Second),
+					wait.WithImmediate(),
+				)
+				require.NoError(t, err)
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentExists(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentExists(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentExists").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentExists", "Deployment should exist", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var dep appsv1.Deployment
+		AssessWithDescription(
+			"deploymentExists",
+			"Deployment should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var dep appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &dep)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &dep)
+				require.NoError(t, err)
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentAvailable(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentAvailable(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentAvailable").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentAvailable", "Deployment should be available", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			err := wait.For(
-				conditions.New(cfg.Client().Resources()).DeploymentAvailable(deploymentName, namespaceName),
-				wait.WithTimeout(2*time.Minute),
-				wait.WithImmediate(),
-			)
-			require.NoError(t, err)
+		AssessWithDescription(
+			"deploymentAvailable",
+			"Deployment should be available",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				err := wait.For(
+					conditions.New(cfg.Client().Resources()).DeploymentAvailable(deploymentName, namespaceName),
+					wait.WithTimeout(2*time.Minute),
+					wait.WithImmediate(),
+				)
+				require.NoError(t, err)
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func SecretExists(namespaceName string, secretName string) e2etypes.Feature {
+func SecretExists(namespaceName, secretName string) e2etypes.Feature {
 	return features.New("SecretExists").
 		WithLabel("type", "secret").
-		AssessWithDescription("secretExists", "Secret should exist", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var sec corev1.Secret
+		AssessWithDescription(
+			"secretExists",
+			"Secret should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var sec corev1.Secret
 
-			err := cfg.Client().Resources("secrets").WithNamespace(namespaceName).Get(ctx, secretName, namespaceName, &sec)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("secrets").
+					WithNamespace(namespaceName).
+					Get(ctx, secretName, namespaceName, &sec)
+				require.NoError(t, err)
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func SecretHasContent(namespaceName string, secretName string, content map[string]string) e2etypes.Feature {
+func SecretHasContent(namespaceName, secretName string, content map[string]string) e2etypes.Feature {
 	return features.New("SecretHasContent").
 		WithLabel("type", "secret").
-		AssessWithDescription("secretHasContent", "Secret should have content", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var sec corev1.Secret
+		AssessWithDescription(
+			"secretHasContent",
+			"Secret should have content",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var sec corev1.Secret
 
-			err := cfg.Client().Resources("secrets").WithNamespace(namespaceName).Get(ctx, secretName, namespaceName, &sec)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("secrets").
+					WithNamespace(namespaceName).
+					Get(ctx, secretName, namespaceName, &sec)
+				require.NoError(t, err)
 
-			for k, v := range content {
-				secData, exists := sec.Data[k]
+				for k, v := range content {
+					secData, exists := sec.Data[k]
 
-				require.True(t, exists)
-				assert.Equal(t, v, string(secData))
-			}
+					require.True(t, exists)
+					assert.Equal(t, v, string(secData))
+				}
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentIsSystemClusterCritical(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentIsSystemClusterCritical(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentIsSystemClusterCritical").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentIsSystemClusterCritical", "Deployment should be system-cluster-critical priority", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var deploy appsv1.Deployment
+		AssessWithDescription(
+			"deploymentIsSystemClusterCritical",
+			"Deployment should be system-cluster-critical priority",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var deploy appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &deploy)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &deploy)
+				require.NoError(t, err)
 
-			assert.Equal(t, "system-cluster-critical", deploy.Spec.Template.Spec.PriorityClassName)
+				assert.Equal(t, "system-cluster-critical", deploy.Spec.Template.Spec.PriorityClassName)
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentHasNoCPULimits(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentHasNoCPULimits(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentHasNoCPULimits").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentHasNoCPULimits", "Deployment should have no CPU limits", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var deploy appsv1.Deployment
+		AssessWithDescription(
+			"deploymentHasNoCPULimits",
+			"Deployment should have no CPU limits",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var deploy appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &deploy)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &deploy)
+				require.NoError(t, err)
 
-			for _, container := range deploy.Spec.Template.Spec.Containers {
-				assert.True(t, container.Resources.Limits.Cpu().IsZero())
-			}
+				for _, container := range deploy.Spec.Template.Spec.Containers {
+					assert.True(t, container.Resources.Limits.Cpu().IsZero())
+				}
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentHasMemoryLimitsEqualToRequests(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentHasMemoryLimitsEqualToRequests(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentHasMemoryLimitsEqualToRequests").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentHasMemoryLimitsEqualToRequests", "Deployment should have memory limits equal to requests", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var deploy appsv1.Deployment
+		AssessWithDescription(
+			"deploymentHasMemoryLimitsEqualToRequests",
+			"Deployment should have memory limits equal to requests",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var deploy appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &deploy)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &deploy)
+				require.NoError(t, err)
 
-			for _, container := range deploy.Spec.Template.Spec.Containers {
-				assert.NotNil(t, container.Resources.Limits.Memory())
-				assert.Equal(t, container.Resources.Requests.Memory(), container.Resources.Limits.Memory())
-			}
+				for _, container := range deploy.Spec.Template.Spec.Containers {
+					assert.NotNil(t, container.Resources.Limits.Memory())
+					assert.Equal(t, container.Resources.Requests.Memory(), container.Resources.Limits.Memory())
+				}
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentHasMemoryRequests(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentHasMemoryRequests(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentHasMemoryRequests").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentHasMemoryRequests", "Deployment should have memory requests", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var deploy appsv1.Deployment
+		AssessWithDescription(
+			"deploymentHasMemoryRequests",
+			"Deployment should have memory requests",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var deploy appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &deploy)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &deploy)
+				require.NoError(t, err)
 
-			for _, container := range deploy.Spec.Template.Spec.Containers {
-				assert.False(t, container.Resources.Requests.Memory().IsZero())
-			}
+				for _, container := range deploy.Spec.Template.Spec.Containers {
+					assert.False(t, container.Resources.Requests.Memory().IsZero())
+				}
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func DeploymentHasCPURequests(namespaceName string, deploymentName string) e2etypes.Feature {
+func DeploymentHasCPURequests(namespaceName, deploymentName string) e2etypes.Feature {
 	return features.New("DeploymentHasCPURequests").
 		WithLabel("type", "deployment").
-		AssessWithDescription("deploymentHasCPURequests", "Deployment should have CPU requests", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var deploy appsv1.Deployment
+		AssessWithDescription(
+			"deploymentHasCPURequests",
+			"Deployment should have CPU requests",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var deploy appsv1.Deployment
 
-			err := cfg.Client().Resources("deployments").WithNamespace(namespaceName).Get(ctx, deploymentName, namespaceName, &deploy)
-			require.NoError(t, err)
+				err := cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deploymentName, namespaceName, &deploy)
+				require.NoError(t, err)
 
-			for _, container := range deploy.Spec.Template.Spec.Containers {
-				assert.False(t, container.Resources.Requests.Cpu().IsZero())
-			}
+				for _, container := range deploy.Spec.Template.Spec.Containers {
+					assert.False(t, container.Resources.Requests.Cpu().IsZero())
+				}
 
-			return ctx
-		}).
+				return ctx
+			}).
 		Feature()
 }
 
-func CRDExists(crdName string, crdVersion string) e2etypes.Feature {
+func CRDExists(crdName, crdVersion string) e2etypes.Feature {
 	return features.New("CRDExists").
 		WithLabel("type", "crd").
-		AssessWithDescription("crdExists", "CRD should exist", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			var crd extv1.CustomResourceDefinition
+		AssessWithDescription(
+			"crdExists",
+			"CRD should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var crd extv1.CustomResourceDefinition
 
-			klient, err := cfg.NewClient()
-			require.NoError(t, err)
+				klient, err := cfg.NewClient()
+				require.NoError(t, err)
 
-			client, err := dynamic.NewForConfig(klient.RESTConfig())
-			require.NoError(t, err)
+				client, err := dynamic.NewForConfig(klient.RESTConfig())
+				require.NoError(t, err)
 
-			unstructuredCRD, err := client.
-				Resource(extv1.SchemeGroupVersion.WithResource("customresourcedefinitions")).
-				Get(ctx, crdName, metav1.GetOptions{})
-			require.NoError(t, err)
+				unstructuredCRD, err := client.
+					Resource(extv1.SchemeGroupVersion.WithResource("customresourcedefinitions")).
+					Get(ctx, crdName, metav1.GetOptions{})
+				require.NoError(t, err)
 
-			err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredCRD.UnstructuredContent(), &crd)
-			require.NoError(t, err)
+				err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredCRD.UnstructuredContent(), &crd)
+				require.NoError(t, err)
 
-			foundVersion := false
-			for _, v := range crd.Spec.Versions {
-				if crdVersion == v.Name {
-					foundVersion = true
+				foundVersion := false
+				for _, v := range crd.Spec.Versions {
+					if crdVersion == v.Name {
+						foundVersion = true
+					}
 				}
-			}
 
-			assert.True(t, foundVersion)
+				assert.True(t, foundVersion)
 
-			return ctx
-		}).
+				return ctx
+			}).
+		Feature()
+}
+
+func PodDisruptionBudgetExists(namespaceName, pdbName string) e2etypes.Feature {
+	return features.New("PodDisruptionBudgetExists").
+		WithLabel("type", "pdb").
+		AssessWithDescription(
+			"pdbExists",
+			"PDB should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var pdb policyv1.PodDisruptionBudget
+
+				err := cfg.Client().
+					Resources("poddisruptionbudgets").
+					WithNamespace(namespaceName).
+					Get(ctx, pdbName, namespaceName, &pdb)
+				require.NoError(t, err)
+
+				return ctx
+			}).
+		Feature()
+}
+
+func PodDisruptionBudgetTargetsDeployment(namespaceName, pdbName, deployName string) e2etypes.Feature {
+	return features.New("PodDisruptionBudgetTargetsDeployment").
+		WithLabel("type", "pdb").
+		AssessWithDescription(
+			"pdbTargetsDeployment",
+			"PDB should target deployment",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var pdb policyv1.PodDisruptionBudget
+				var deploy appsv1.Deployment
+
+				err := cfg.Client().
+					Resources("poddisruptionbudgets").
+					WithNamespace(namespaceName).
+					Get(ctx, pdbName, namespaceName, &pdb)
+				require.NoError(t, err)
+
+				err = cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deployName, namespaceName, &deploy)
+				require.NoError(t, err)
+
+				for labelKey, labelValue := range pdb.Spec.Selector.MatchLabels {
+					require.Equal(t, deploy.Spec.Selector.MatchLabels, labelKey)
+					require.Equal(t, deploy.Spec.Selector.MatchLabels[labelKey], labelValue)
+				}
+
+				return ctx
+			}).
 		Feature()
 }
